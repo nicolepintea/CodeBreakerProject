452173f1d7c1c5c45f62e51fab12a535a26c867b
nicolepintea <nicole.pintea@gmail.com>
2017-11-21 11:06:38 +0100

    Added forst function setHiddenFields (5 done)

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,4 +6,10 @@ function guess() {
     //add functionality to guess function here
 }
 
+function setHiddenFields(){
+    answet=Math.floor(Math.random()*10000);
+    while(answer.nodeValue.length<4){
+        answer.value+=0+answer.value.toString();
+    }
+}
 //implement new functions here
